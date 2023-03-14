dc027b67cbaa7928fcdd2a4bb7e7dc3a6fac2561
Christy Le <lechrsty@gmail.com>
2023-03-13 19:50:34 -0700

    GET all albums, single albums, and query albums by taste__id

---
+++ server/vinylcutapi/views/album_view.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from vinylcutapi.models import Album, Genre, Taste
+
+
+class AlbumView(ViewSet):
+
+    def retrieve(self, request, pk):
+
+        album = Album.objects.get(pk=pk)
+        serializer = AlbumSerializer(album)
+        return Response(serializer.data)
+
+
+    def list(self, request):
+            # get the Taste from the request query params
+            taste = request.query_params.get('taste')
+            
+            if taste:
+                # filter the Album queryset by the specified Taste id
+                albums = Album.objects.filter(taste__id=taste)
+            else:
+                # get all albums
+                albums = Album.objects.all()
+            
+            serializer = AlbumSerializer(albums, many=True)
+            return Response(serializer.data)
+
+
+class AlbumGenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('type', )
+
+class AlbumSerializer(serializers.ModelSerializer):
+
+    genre = AlbumGenreSerializer(many=False)
+
+    class Meta:
+        model = Album
+        fields = ('id', 'title', 'artist', 'description', 'image_url', 'genre', )
