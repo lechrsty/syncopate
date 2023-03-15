31928cb0592dfab8e359b6e77bcf2341e9886419
Christy Le <lechrsty@gmail.com>
2023-03-15 14:54:13 -0700

    GET all genres and ratings

---
+++ server/vinylcutapi/views/rating_view.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from vinylcutapi.models import Rating
+
+class RatingView(ViewSet):
+
+    def list(self, request):
+
+        ratings = Rating.objects.all()
+        serializer = RatingSerializer(ratings, many=True)
+
+        return Response(serializer.data)
+
+class RatingSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Rating
+        fields = ('id', 'rating',  )
