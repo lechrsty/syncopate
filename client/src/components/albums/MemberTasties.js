60665f4dd0f54718838be27f43c52daaffa7ae75
Christy Le <lechrsty@gmail.com>
2023-03-17 23:42:14 -0700

    member can register with a taste selection

---
+++ client/src/components/albums/MemberTasties.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TasteAlbumList } from './TasteAlbumList';
+
+export const MemberTasties = () => {
+
+    const localVinylCutUser = localStorage.getItem("vinylcut")
+    const vinylCutUserObject = JSON.parse(localVinylCutUser)
+    
+    return <TasteAlbumList tasteId="4" />;
+};
--- client/src/managers/TasteManager.js
+++ client/src/managers/TasteManager.js
@@ -9,3 +9,12 @@ export const getTastes = () => {
     })
     .then((res) => res.json())
 }
+
+export const registerTastes = () => {
+    return fetch("http://localhost:8000/tastes", {
+        headers: {
+            "Authorization" : `Token 0be249c88238743e5b4a7ac370b5145730c28e20`
+        }
+    })
+    .then((res) => res.json())
+}
